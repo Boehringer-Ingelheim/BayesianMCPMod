713305613907c7761ad09c59bc23f8a34b85dea5
stevegbrooks <stevegbrooks@gmail.com>
2023-11-06 13:57:15 +0800

    update bmcpmod tests

--- tests/testthat/test-BMCPMod.R
+++ tests/testthat/test-BMCPMod.R
@@ -1,58 +1,35 @@
-# Import functions to test 
-source("R/BMCPMod.R")
+# Load the necessary library
+library(testthat)
 
-# Import testthat for unit testing
-library(testthat) 
+# Define the context for the tests
+context("Tests for assessDesign function")
 
-# Tests for assessDesign
-test_that("assessDesign returns correct class", {
-  expect_s3_class(assessDesign(n_patients, mods, prior_list), "BayesianMCPMod") 
+# Test for n_patients parameter
+test_that("n_patients parameter works correctly", {
+  # Create a test case
+  test_n_patients <- c(10, 20, 30)
+  # Call the function with the test case
+  result <- assessDesign(n_patients = test_n_patients, mods = NULL, prior_list = NULL)
+  # Assert the expected behavior
+  expect_equal(length(result$n_patients), length(test_n_patients))
 })
 
-test_that("assessDesign handles errors", {
-  expect_error(assessDesign(n_patients, mods, prior_list, n_sim = "string"))
-  expect_error(assessDesign(n_patients, mods, prior_list, alpha_crit_val = -1))
+# Test for mods parameter
+test_that("mods parameter works correctly", {
+  # Create a test case
+  test_mods <- Mods()
+  # Call the function with the test case
+  result <- assessDesign(n_patients = NULL, mods = test_mods, prior_list = NULL)
+  # Assert the expected behavior
+  expect_equal(class(result$mods), "Mods")
 })
 
-# Tests for getContrMat
-test_that("getContrMat returns correct class", {
-  expect_s3_class(getContrMat(mods, dose_levels, dose_weights, prior_list), "optContr")
-})
-
-test_that("getContrMat handles errors", {
-  expect_error(getContrMat(mods, dose_levels, dose_weights, prior_list = 1))
-})
-
-# Tests for getCritProb
-test_that("getCritProb returns a probability", {
-  expect_gte(getCritProb(mods, dose_levels, dose_weights), 0)
-  expect_lte(getCritProb(mods, dose_levels, dose_weights), 1)  
-})
-
-test_that("getCritProb handles errors", {
-  expect_error(getCritProb(mods, dose_levels, dose_weights, alpha_crit_val = -1))
-})
-
-# Tests for performBayesianMCPMod
-test_that("performBayesianMCPMod returns correct class", {
-  expect_s3_class(performBayesianMCPMod(posteriors_list, contr_mat, crit_prob), "BayesianMCPMod")
-})
-
-test_that("performBayesianMCPMod handles errors", {
-  expect_error(performBayesianMCPMod(posteriors_list, contr_mat, crit_prob = -1))
-})
-
-# Tests for addSignificance
-test_that("addSignificance adds significant attribute", {
-  model_fits <- list(a = 1, b = 2)
-  expect_named(addSignificance(model_fits, c(TRUE, FALSE)), "significant") 
-})
-
-# Tests for performBayesianMCP
-test_that("performBayesianMCP returns correct class", {
-  expect_s3_class(performBayesianMCP(posteriors_list, contr_mat, crit_prob), "BayesianMCP")  
-})
-
-test_that("performBayesianMCP handles errors", {
-  expect_error(performBayesianMCP(posteriors_list, contr_mat, crit_prob = -1))
+# Test for prior_list parameter
+test_that("prior_list parameter works correctly", {
+  # Create a test case
+  test_prior_list <- list()
+  # Call the function with the test case
+  result <- assessDesign(n_patients = NULL, mods = NULL, prior_list = test_prior_list)
+  # Assert the expected behavior
+  expect_equal(class(result$prior_list), "list")
 })
