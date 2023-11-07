908f80d74476c0fd37177ce64273c1668850aff4
Brooks <steven.brooks@boehringer-ingelheim.com>
2023-11-07 15:57:46 +0800

    finished n_patients validaiton tests

---
+++ tests/testthat/setup.R
@@ -0,0 +1,25 @@
+
+# Create minimal test case
+n_hist_trials = 1
+
+hist_data <- data.frame(
+  trial = seq(1, n_hist_trials, 1),
+  est   = rep(1, n_hist_trials),
+  se    = rep(1, n_hist_trials),
+  sd    = rep(1, n_hist_trials),
+  n     = rep(1, n_hist_trials)
+)
+
+n_patients <- c(2, 1.3)
+dose_levels <- c(0, 2.5)
+
+mods <- DoseFinding::Mods(
+  linear = NULL, 
+  doses = dose_levels
+)
+
+prior_list <- getPriorList(
+  hist_data   = hist_data,
+  dose_levels = dose_levels,
+  robustify_weight = 0.5
+)
--- tests/testthat/test-BMCPMod.R
+++ tests/testthat/test-BMCPMod.R
@@ -1,72 +1,8 @@
-# Load the necessary library
-library(testthat)
+##########################
+# Tests for assessDesign #
+##########################
 
-# Create minimal test case
-n_hist_trials = 1
-
-hist_data <- data.frame(
-  trial = seq(1, n_hist_trials, 1),
-  est   = rep(-1, n_hist_trials),
-  se    = rep(1, n_hist_trials),
-  sd    = rep(1, n_hist_trials),
-  n     = rep(1, n_hist_trials)
-)
-
-n_patients <- c(2, 2)
-dose_levels <- c(0, 2.5)
-
-mods <- DoseFinding::Mods(
-  linear = NULL, 
-  doses = dose_levels
-)
-
-prior_list <- getPriorList(
-  hist_data   = hist_data,
-  dose_levels = dose_levels,
-  robustify_weight = 0.5
-)
-
-# Tests for n_patients parameter
-test_that("assessDesign correctly validates n_patients parameter input", {
-
-  expect_error(
-    assessDesign(
-      n_patients = NULL, 
-      mods = mods, 
-      prior_list = prior_list
-    ),
-    "not be NULL",
-    ignore.case = T
-  )
-  
-  expect_error(
-    assessDesign(
-      n_patients = n_patients[-1], 
-      mods = mods, 
-      prior_list = prior_list
-    ),
-    "match number of dose levels",
-    ignore.case = T
-  )
-  
-  expect_error(
-    assessDesign(
-      n_patients = rep(1, length(n_patients)),
-      mods = mods,
-      prior_list = prior_list
-    ),
-    "at least one value must be greater than 1",
-    ignore.case = T
-  )
-  
-  expect_no_error(
-    assessDesign(
-      n_patients = n_patients - c(0, 1),
-      mods = mods,
-      prior_list = prior_list
-    )
-  )
-
+test_that("base case input throws no error", {
   expect_no_error(
     assessDesign(
       n_patients = n_patients, 
@@ -75,25 +11,43 @@ test_that("assessDesign correctly validates n_patients parameter input", {
     )
   )
 })
-# 
-# # Test for mods parameter
-# test_that("mods parameter works correctly", {
-#   # Call the function with the test case
-#   result <- assessDesign(
-#     n_patients = n_patients, 
-#     mods = mods, 
-#     prior_list = prior_list
-#   )
-#   # Assert the expected behavior
-#   expect_equal(class(result$mods), "Mods")
-# })
-# 
-# # Test for prior_list parameter
-# test_that("prior_list parameter works correctly", {
-#   # Create a test case
-#   test_prior_list <- list()
-#   # Call the function with the test case
-#   result <- assessDesign(n_patients = NULL, mods = NULL, prior_list = test_prior_list)
-#   # Assert the expected behavior
-#   expect_equal(class(result$prior_list), "list")
-# })
+
+
+### n_patients parameter ###
+test_that("assessDesign validates n_patients parameter input and give appropriate error messages", {
+
+  expect_error(
+    assessDesign(n_patients = NULL, mods = mods, prior_list = prior_list),
+    "n_patients should not be NULL", ignore.case = T
+  )
+  
+  expect_error(
+    assessDesign(n_patients = list(), mods = mods, prior_list = prior_list),
+    "n_patients should be a vector", ignore.case = T
+  )
+  
+  expect_error(
+    assessDesign(n_patients = c("2", "2"), mods = mods, prior_list = prior_list),
+    "n_patients should be numeric", ignore.case = T
+  )
+  
+  expect_error(
+    assessDesign(n_patients = n_patients[-1], mods = mods, prior_list = prior_list),
+    "length of n_patients should equal number of dose groups", ignore.case = T
+  )
+  
+  expect_error(
+    assessDesign(n_patients = rep(1, length(n_patients)), mods = mods, prior_list = prior_list),
+    "at least one element in n_patients needs to be > 1", ignore.case = T
+  )
+})
+
+### mods parameter ###
+test_that("assessDesign validates mods parameter input and give appropriate error messages", {
+  
+  # assertions that aren't tested here for sake of brevity
+    # mods should not be NULL
+    # mods should be of class "Mods" from {DoseFinding}
+    # length(n_patients) == length(attributes(mods)$doses) is commutative, so testing here is redundant
+  
+})
