5a59a4bc6ad643049ff608db129826392da3f438
Schick <jonas.schick@boehringer-ingelheim.com>
2024-05-16 11:10:35 +0200

    - renamed getPosteriorOutput() function to postmix2RBesT() and updated input parameters
    - updated postmix2RBesT() to implement more efficient coding methods
    
    - extracted ESS calculation steps into its own function calcESS() in order to avoid redundant code

--- R/posterior.R
+++ R/posterior.R
@@ -8,7 +8,7 @@
 #' @param data dataframe containing the information of dose and response. Default NULL
 #' Also a simulateData object can be provided.
 #' @param mu_hat vector of estimated mean values (per dose group).
-#' @param se_hat vector of estimated standard deviations (per dose group).
+#' @param S_hat vector or matrix of estimated standard deviations (per dose group).
 #' @param calc_ess boolean variable, indicating whether effective sample size should be calculated. Default FALSE
 #' @return posterior_list, a posterior list object is returned with information about (mixture) posterior distribution per dose group
 #' @examples
@@ -33,7 +33,7 @@ getPosterior <- function(
   prior_list,
   data     = NULL,
   mu_hat   = NULL,
-  se_hat   = NULL,
+  S_hat    = NULL,
   calc_ess = FALSE
   
 ) {
@@ -42,36 +42,36 @@ getPosterior <- function(
   checkmate::check_list(prior_list, names = "named", any.missing = FALSE)
   checkmate::check_vector(mu_hat, any.missing = FALSE, null.ok = TRUE)
   checkmate::check_double(mu_hat, null.ok = TRUE, lower = -Inf, upper = Inf)
-  checkmate::check_vector(se_hat, any.missing = FALSE, null.ok = TRUE)
-  checkmate::check_double(se_hat, null.ok = TRUE, lower = 0, upper = Inf)
+  checkmate::check_vector(S_hat, any.missing = FALSE, null.ok = TRUE)
+  checkmate::check_double(S_hat, null.ok = TRUE, lower = 0, upper = Inf)
   
-  if (!is.null(mu_hat) && !is.null(se_hat) && is.null(data)) {
+  if (!is.null(mu_hat) && !is.null(S_hat) && is.null(data)) {
     
-    if (dim(se_hat)[2] > 1 && dim(se_hat)[2] == length(prior_list)) {
-      
+    if (dim(S_hat)[2] > 1 && dim(S_hat)[2] == length(prior_list)) {
       prior_mix <- createPriorMix(prior_list)
       
       posterior <- DoseFinding::mvpostmix(
-        priormix  = prior_mix,
-        mu_hat    = mu_hat,
-        S_hat    = se_hat)
+        priormix = prior_mix,
+        mu_hat   = mu_hat,
+        S_hat    = S_hat)
       
-      posterior_list <- getPosteriorOutput(
+      posterior_list <- postmix2RBesT(
         posterior_list = posterior, 
         prior_list     = prior_list, 
+        prior_mix      = prior_mix, 
         calc_ess       = calc_ess)
       
-    } else if (dim(se_hat)[2] == 1) {
+    } else if (dim(S_hat)[2] == 1) {
       
       posterior_list <- getPosteriorI(
         prior_list = prior_list,
         mu_hat     = mu_hat,
-        se_hat     = se_hat,
+        se_hat     = S_hat,
         calc_ess   = calc_ess)
       
     }
     
-  } else if (is.null(mu_hat) && is.null(se_hat) && !is.null(data)) {
+  } else if (is.null(mu_hat) && is.null(S_hat) && !is.null(data)) {
     
     posterior_list <- lapply(split(data, data$simulation), getPosteriorI,
                              prior_list = prior_list, calc_ess = calc_ess)
@@ -144,18 +144,10 @@ getPosteriorI <- function(
   names(post_list) <- names(prior_list)
   class(post_list) <- "postList"
   
-  if (calc_ess) {
-    
-    attr(post_list, "ess") <- getESS(post_list)
-    
-  } else {
-    
-    attr(post_list, "ess") <- numeric(0)
-    
-  }
+  attr(post_list, "ess") <- calcEss(calc_ess, post_list)
   
   attr(post_list, "full covariance matrices") <- replicate(length(prior_list)-1, diag(c(se_hat)), simplify = FALSE)
-  names(attr(post_list, "full covariance matrices")) <- c("comp1", "comp2", "comp3", "robust")
+  #names(attr(post_list, "full covariance matrices")) <- c("comp1", "comp2", "comp3", "robust")
   
   return (post_list)
   
@@ -199,17 +191,9 @@ getPostCombsI <- function (
   
 }
 
-################################################################################
-# @all: content of createPriorMix() is from DoseFinding (see: bMCTtest.R lines 73-88)
-#       mvpostmix() has been migrated as is from DoseFinding package
-#       getPosteriorOutput() turns posterior object from mvpostmix() into RBesT type object
-#       new variables have been defined in setup.R for new tests
-#       new tests for all new functions have been added in test-posterior.R
-# -> please check out code & outputs and make changes if necessary
-
 createPriorMix <- function(prior) {
   
-  checkmate::check_list(prior, names = "named", any.missing = FALSE, null.ok = FALSE)
+  checkmate::assert_list(prior, names = "named", any.missing = FALSE, null.ok = FALSE)
   
   k <- length(prior)
   
@@ -233,56 +217,86 @@ createPriorMix <- function(prior) {
   
 }
  
-getPosteriorOutput <- function(
+postmix2RBesT <- function(
     
   posterior_list,
   prior_list,
+  prior_mix,
   calc_ess = FALSE
   
 ) {
   
-  checkmate::check_list(posterior_list, names = "named", any.missing = FALSE, null.ok = FALSE)
-  checkmate::check_list(prior_list, names = "named", any.missing = FALSE, null.ok = FALSE)
+  checkmate::assert_list(posterior_list, names = "named", any.missing = FALSE, null.ok = FALSE)
+  checkmate::assert_list(prior_list, names = "named", any.missing = FALSE, null.ok = FALSE)
   
+  summed_values <- vector("list", length(prior_list))
   posterior_output <- vector("list", length(prior_list))
-  names(posterior_output) <- names(prior_list)
   
-  posterior_list_weights <- t(posterior_list$weights)
-  posterior_list_means <- t(sapply(posterior_list$mean, rbind))
-  ctr_data <- rbind(rbind(posterior_list_weights, posterior_list_means[,1]), sapply(1:(length(posterior_list$covmat)), function(x) posterior_list$covmat[[x]][1,1]))
-  
-  pos_data_frame <- as.data.frame(ctr_data)
-  
-  posterior_ctr <- mixnorm(unlist(pos_data_frame$Comp1), 
-                           unlist(pos_data_frame$Comp2), 
-                           unlist(pos_data_frame$Comp3), 
-                           unlist(pos_data_frame$Comp4), 
-                           sigma = sigma(prior_list$Ctr))
-  
-  colnames(posterior_ctr)[4] <- "robust"
-  posterior_output[[1]] <- posterior_ctr
-
-  posterior_output[2:length(prior_list)] <- lapply(2:length(posterior_output), function(x) {
+  posterior_output <- lapply(seq_along(prior_list), function(i) {
     
-    covmat_sum <- sum(sapply(1:4, function(y) posterior_list$covmat[[y]][x,x]))
-    mixnorm(comp1 = c(sum(unlist(posterior_list$weights)), sum(unlist(posterior_list_means[,x])), covmat_sum), sigma = sigma(prior_list$DG_1))
+    patterns <- lapply(prior_list[[i]]["m",], function(x) {
+      
+      matches <- sapply(seq_along(posterior_list$weights), function(j) {
+        
+        prior_mix[[2]][[j]][i] == x
+        
+      })
+      
+      result <- which(matches)
+      
+      return(result)
+      
+    })
+    
+    diagonals <- lapply(posterior_list$covmat, diag)
+    diagonals_bound <- sapply(diagonals, cbind)
+    
+    summed_values <- lapply(patterns, function(x) {
+      
+      c(sum(as.numeric(t(t(posterior_list$weights[x])))),
+        mean(as.numeric(t(sapply(posterior_list$mean, rbind))[x,i])),
+        mean(diagonals_bound[i,x])
+      )
+      
+    })
+    
+    additional_args <- list(sigma = sigma(prior_list[[i]]))
+    
+    args <- c(summed_values, additional_args)
+    
+    result <- do.call(RBesT::mixnorm, args)
+    
+    return(result)
     
   })
   
+  names(posterior_output) <- c("Ctr", paste0("DG_", seq_along(posterior_output[-1])))
+  
   class(posterior_output) <- "postList"
   
-  if (calc_ess) {
-    
-    attr(posterior_output, "ess") <- getESS(posterior_output)
-    
-  } else {
-    
-    attr(posterior_output, "ess") <- numeric(0)
-    
-  }
+  attr(posterior_output, "ess") <- calcEss(calc_ess, posterior_output)
   
   attr(posterior_output, "full covariance matrices") <- posterior_list$covmat
   
   return(posterior_output)
+  
+}
+
+calcEss <- function(calc_ess, posterior_output) {
+  
+  checkmate::assert_logical(calc_ess, null.ok = FALSE, len = 1)
+  checkmate::assert_list(posterior_output, names = "named", any.missing = FALSE, null.ok = FALSE)
+  
+  if (calc_ess) {
+    
+    post_ESS <- getESS(posterior_output)
+    
+  } else {
+    
+    post_ESS <- numeric(0)
+    
+  }
+  
+  return(post_ESS)
   
 }
