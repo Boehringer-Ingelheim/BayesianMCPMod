73228fad2b2c70d675b58dffc148c7eeeb62024e
Andersen <andersla@inter-standard-dy-cpr-interactive-st-13.workbenchhpc5795f00e20240614073305.pcluster>
2024-06-28 12:29:23 +0200

    fixing minor things

--- R/modeling.R
+++ R/modeling.R
@@ -1,12 +1,12 @@
 #' @title getModelFits
-#' 
+#'
 #' @description Fits dose-response curves for the specified dose-response models, based on the posterior distributions.
-#' For the simplified fit, multivariate normal distributions will be approximated and reduced by one-dimensional normal distributions. 
-#' For the default case, the Nelder-Mead algorithm is used. 
+#' For the simplified fit, multivariate normal distributions will be approximated and reduced by one-dimensional normal distributions.
+#' For the default case, the Nelder-Mead algorithm is used.
 #' In detail, for both approaches the mean vector \eqn{\theta^{Y}} and the covariance \eqn{\Sigma} of the (mixture) posterior distributions and the corresponding posterior weights \eqn{\tilde{\omega}_{l}} for \eqn{l \in {1,...,L}} are used as basis
 #' For the full fit a GLS estimator is used to minimize the following expression for the respective dose-response models \eqn{m}
 #' \deqn{ \hat{\theta}_{m}=argmin_{\theta_{m}} \sum_{l=1}^{L} \tilde{\omega}_{l}(\theta_{l_{i}}^{Y}-f(dose_{i},\hat{\theta}_{m}))'\Sigma_{l}^{-1}(\theta_{l_{i}}^{Y}-f(dose_{i},\hat{\theta}_{m}))}
-#' Therefore the function nloptr of the nloptr package is utilized. 
+#' Therefore the function nloptr of the nloptr package is utilized.
 #' In the simplified case \eqn{L=1}, as the dimension of the posterior is reduced to 1 first.
 #' The generalized AIC values are calculated via the formula
 #' \deqn{gAIC_{m} = \sum_{l=1}^{L} \tilde{\omega}_{l} \sum_{i=0}^{K} \frac{1}{\Sigma_{l_{i,i}}} (\theta_{l_i}^Y - f(dose_{i},\hat{\theta}_{m}))^2 + 2p }
@@ -20,17 +20,17 @@
 #' @references Schorning K, Bornkamp B, Bretz F, Dette H. 2016. Model selection versus model averaging in dose finding studies. Stat Med; 35; 4021-4040.
 #' @param models List of model names for which a fit will be performed.
 #' @param dose_levels A vector containing the different dosage levels.
-#' @param posterior A getPosterior object, containing the (multivariate) posterior distribution per dosage level. 
+#' @param posterior A getPosterior object, containing the (multivariate) posterior distribution per dosage level.
 #' @param simple Boolean variable, defining whether simplified fit will be applied. Default FALSE.
 #' @examples
 #' posterior_list <- list(Ctrl = RBesT::mixnorm(comp1 = c(w = 1, m = 0, s = 1), sigma = 2),
 #'                        DG_1 = RBesT::mixnorm(comp1 = c(w = 1, m = 3, s = 1.2), sigma = 2),
-#'                        DG_2 = RBesT::mixnorm(comp1 = c(w = 1, m = 4, s = 1.5), sigma = 2) ,  
+#'                        DG_2 = RBesT::mixnorm(comp1 = c(w = 1, m = 4, s = 1.5), sigma = 2) ,
 #'                        DG_3 = RBesT::mixnorm(comp1 = c(w = 1, m = 6, s = 1.2), sigma = 2) ,
 #'                        DG_4 = RBesT::mixnorm(comp1 = c(w = 1, m = 6.5, s = 1.1), sigma = 2))
 #' models         <- c("emax", "exponential", "sigEmax", "linear")
 #' dose_levels    <- c(0, 1, 2, 4, 8)
-#' 
+#'
 #' fit        <- getModelFits(models      = models,
 #'                            posterior   = posterior_list,
 #'                            dose_levels = dose_levels)
@@ -38,17 +38,17 @@
 #'                            posterior   = posterior_list,
 #'                            dose_levels = dose_levels,
 #'                            simple      = TRUE)
-#'                            
+#'
 #' @return An object of class modelFits. A list containing information about the fitted model coefficients, the prediction per dose group as well as maximum effect and generalized AIC (and corresponding weight) per model.
-#' 
+#'
 #' @export
 getModelFits <- function (
-    
+
   models,
   dose_levels,
   posterior,
   simple = FALSE
-  
+
 ) {
   if (inherits(models, "character")) {
     models <- stats::setNames(as.list(models), models)
@@ -57,33 +57,33 @@ getModelFits <- function (
   checkmate::check_double(dose_levels, lower = 0, any.missing = FALSE, len = length(models))
   checkmate::check_class(posterior, "postList")
   checkmate::check_logical(simple)
-  
+
   model_names <- unique(gsub("\\d", "", names(models)))
 
   getModelFit <- ifelse(simple, getModelFitSimple, getModelFitOpt)
 
   model_fits  <- lapply(model_names, getModelFit, dose_levels, posterior, list("scal" = attr(models, "scal")))
   model_fits  <- addModelWeights(model_fits)
-  
+
   names(model_fits)             <- model_names
   attr(model_fits, "posterior") <- posterior
   class(model_fits)             <- "modelFits"
-  
-  
+
+
   return (model_fits)
-  
+
 }
 
 ## ignoring mixture posterior
 getModelFitSimple <- function (
-    
+
   model,
   dose_levels,
   posterior,
   addArgs = NULL
-  
+
 ) {
-  
+
   fit <- DoseFinding::fitMod(
     dose  = dose_levels,
     resp  = summary.postList(posterior)[, 1],
@@ -91,11 +91,11 @@ getModelFitSimple <- function (
     model = model,
     type  = "general",
     bnds  = DoseFinding::defBnds(mD = max(dose_levels))[[model]])
-  
+
   pred_vals  <- stats::predict(fit, predType = "ls-means")
   max_effect <- max(pred_vals) - min(pred_vals)
   gAIC       <- DoseFinding::gAIC(fit)
-  
+
   model_fit <- list(
     model       = model,
     coeffs      = fit$coefs,
@@ -103,28 +103,28 @@ getModelFitSimple <- function (
     pred_values = pred_vals,
     max_effect  = max_effect,
     gAIC        = gAIC)
-  
+
   return (model_fit)
-  
+
 }
 
 ## taking mixture posterior into account
 getModelFitOpt <- function (
-    
+
   model,
   dose_levels,
   posterior,
   addArgs = NULL
-  
+
 ) {
-  
+
   getOptParams <- function (
-    
+
     model,
     dose_levels,
     posterior,
     addArgs = NULL
-    
+
   ) {
     switch (
       model,
@@ -165,18 +165,18 @@ getModelFitOpt <- function (
         expr_i <- substitute(
           sum(
               ((post_combs$means[i, ] - (theta[1] + theta[2] * (((theta[3] + theta[4])^(theta[3] + theta[4])) / ((theta[3] ^ theta[3]) * (theta[4]^theta[4]))) * (dose_levels / scal)^(theta[3]) * ((1 - dose_levels / scal)^(theta[4]))))^2 / (post_combs$vars[i, ])))
-          , 
+          ,
           list(scal = scal)
         )
       },
       stop(paste("error:", model, "shape not yet implemented"))
     )
-    
+
     simple_fit <- getModelFitSimple(
       model       = model,
       dose_levels = dose_levels,
       posterior   = posterior)
-    
+
     param_list <- list(
       expr_i  = expr_i,
       opts    = list("algorithm" = "NLOPT_LN_NELDERMEAD", maxeval = 1e3), #,stopval=0
@@ -185,22 +185,22 @@ getModelFitOpt <- function (
         lb = lb,
         ub = ub),
       c_names = names(simple_fit$coeffs))
-    
+
     return (param_list)
-    
+
   }
-  
+
   optFun <- function (
     theta,
     dose_levels,
     post_combs,
     expr_i
   ) {
-    
+
     comps <- sapply(seq_along(post_combs$weights), function (i) eval(expr_i))
-    
+
     return (sum(post_combs$weights * comps))
-    
+
   }
 
   param_list <- getOptParams(model, dose_levels, posterior)
@@ -216,28 +216,28 @@ getModelFitOpt <- function (
     post_combs  = post_combs,
     expr_i      = param_list$expr_i
   )
-  
+
   names(fit$solution) <- param_list$c_names
-  
+
   model_fit <- list(
     model       = model,
     coeffs      = fit$solution,
     dose_levels = dose_levels)
-  
+
   model_fit$pred_values <- predictModelFit(model_fit = model_fit, doses = model_fit$dose_levels, addArgs = addArgs)
   model_fit$max_effect  <- max(model_fit$pred_values) - min(model_fit$pred_values)
   model_fit$gAIC        <- getGenAIC(model_fit, post_combs)
-  
+
   return (model_fit)
-  
+
 }
 
 predictModelFit <- function (
-    
+
   model_fit,
   doses = NULL,
   addArgs = NULL
-  
+
 ) {
   if (is.null(doses)) {
     doses <- model_fit$dose_levels
@@ -283,44 +283,44 @@ predictModelFit <- function (
       model_fit$coeffs["delta2"],
       ifelse(is.null(addArgs) | is.null(addArgs[["scal"]]), 1.2 * max(doses), addArgs[["scal"]]))},
     {stop(paste("error:", model_fit$model, "shape not yet implemented"))})
-  
+
   return (pred_vals)
-  
+
 }
 
 getGenAIC <- function (
-    
+
   model_fit,
   post_combs
-  
+
 ) {
-  
+
   expr_i   <- quote(sum(1 / post_combs$vars[i, ] * (post_combs$means[i, ] - model_fit$pred_values)^2) + 2 * length(model_fit$coeffs))
   comb_aic <- sapply(seq_along(post_combs$weights), function (i) eval(expr_i))
-  
+
   g_aic    <- stats::weighted.mean(comb_aic, w = post_combs$weights)
-  
+
   return (g_aic)
-  
+
 }
 
 addModelWeights <- function (
-    
+
   model_fits
-  
+
 ) {
   g_aics        <- sapply(model_fits, function (model_fit) model_fit$gAIC)
   exp_values    <- exp(-0.5 * g_aics)
   model_weights <- exp_values / sum(exp_values)
-  
+
   names(model_weights) <- NULL
-  
+
   model_fits_out <- lapply(seq_along(model_fits), function (i) {
-    
+
     c(model_fits[[i]], model_weight = model_weights[i])
-    
+
   })
-  
+
   return (model_fits_out)
-  
+
 }
--- R/s3methods.R
+++ R/s3methods.R
@@ -2,14 +2,14 @@
 
 #' @export
 print.BayesianMCPMod <- function (
-    
+
   x,
   ...
-  
+
 ) {
-  
+
   print(x$BayesianMCP)
-  
+
 }
 
 ## BayesianMCP --------------------------------------------
@@ -17,16 +17,16 @@ print.BayesianMCPMod <- function (
 #' @export
 print.BayesianMCP <- function(x, ...) {
   cat("Bayesian Multiple Comparison Procedure\n\n")
-  
+
   cat("Summary:\n")
   cat("  Sign:", x[1, "sign"], "\n")
   cat("  Critical Probability:", x[1, "crit_prob_adj"], "\n")
   cat("  Maximum Posterior Probability:", x[1, "max_post_prob"], "\n\n")
-  
+  n_sim <- nrow(x)
   cat("Effective Sample Size (ESS) per Dose Group:\n")
   print(attr(x, "ess_avg"), row.names = FALSE)
   cat("\n")
-  
+
   cat("Posterior Probabilities for Model Shapes:\n")
   model_probs <- x[1, grep("^post_probs\\.", colnames(x))]
   model_names <- gsub("post_probs\\.", "", names(model_probs))
@@ -34,63 +34,63 @@ print.BayesianMCP <- function(x, ...) {
   print(model_df, row.names = FALSE)
 
   cat("Bayesian Multiple Comparison Procedure\n")
-  
+
   if (n_sim == 1L) {
-    
+
     attr(x, "critProbAdj") <- NULL
     attr(x, "successRate") <- NULL
     class(x)               <- NULL
-    
+
     print.default(x, ...)
-    
+
     # if (any(!is.na(attr(x, "essAvg")))) {
-    #   
+    #
     #   cat("Average Posterior ESS\n")
     #   print(attr(x, "essAvg"), ...)
-    #   
+    #
     # }
-    
+
   } else {
-    
+
     model_successes <- getModelSuccesses(x)
-    
+
     cat("  Estimated Success Rate: ", attr(x, "successRate"), "\n")
     cat("  N Simulations:          ", n_sim)
-    
+
     cat("\n")
     cat("Model Significance Frequencies\n")
     print(model_successes, ...)
-    
+
   }
-  
+
   invisible(x)
 }
 # print.BayesianMCP <- function (
-    #     
+    #
 #   x,
 #   ...
-#   
+#
 # ) {
-#   
+#
 #   n_sim <- nrow(x)
-#   
+#
 #   cat("Bayesian Multiple Comparison Procedure\n")
-#   
+#
 #   if (n_sim == 1L) {
-#     
+#
 #     attr(x, "crit_prob_adj") <- NULL
 #     attr(x, "success_rate")  <- NULL
 #     class(x) <- NULL
-#     
+#
 #     print.default(x, ...)
-#     
+#
 #   } else {
-#     
+#
 #     cat("  Estimated Success Rate: ", attr(x, "successRate"), "\n")
 #     cat("  N Simulations:          ", n_sim)
-#     
+#
 #   }
-#   
+#
 # }
 
 
@@ -98,17 +98,17 @@ print.BayesianMCP <- function(x, ...) {
 
 #' @title predict.modelFits
 #' @description This function performs model predictions based on the provided
-#' model and dose specifications 
-#' 
+#' model and dose specifications
+#'
 #' @param object A modelFits object containing information about the fitted
 #' model coefficients
 #' @param doses A vector specifying the doses for which a prediction should be
-#' done 
+#' done
 #' @param ... Currently without function
 #' @examples
 #' posterior_list <- list(Ctrl = RBesT::mixnorm(comp1 = c(w = 1, m = 0, s = 1), sigma = 2),
 #'                        DG_1 = RBesT::mixnorm(comp1 = c(w = 1, m = 3, s = 1.2), sigma = 2),
-#'                        DG_2 = RBesT::mixnorm(comp1 = c(w = 1, m = 4, s = 1.5), sigma = 2) ,  
+#'                        DG_2 = RBesT::mixnorm(comp1 = c(w = 1, m = 4, s = 1.5), sigma = 2) ,
 #'                        DG_3 = RBesT::mixnorm(comp1 = c(w = 1, m = 6, s = 1.2), sigma = 2) ,
 #'                        DG_4 = RBesT::mixnorm(comp1 = c(w = 1, m = 6.5, s = 1.1), sigma = 2))
 #' models         <- c("emax", "exponential", "sigEmax", "linear")
@@ -116,56 +116,56 @@ print.BayesianMCP <- function(x, ...) {
 #' fit            <- getModelFits(models      = models,
 #'                                posterior   = posterior_list,
 #'                                dose_levels = dose_levels)
-#'                                
+#'
 #' predict(fit, doses = c(0, 1, 3, 4, 6, 8))
-#' 
+#'
 #' @return a list with the model predictions for the specified models and doses
-#' 
+#'
 #' @export
 predict.modelFits <- function (
-    
+
   object,
   doses = NULL,
   ...
-  
+
 ) {
-  
+
   predictions <- lapply(object, predictModelFit, doses = doses)
   attr(predictions, "doses") <- doses
-  
+
   return (predictions)
-  
+
 }
 
 #' @export
 print.modelFits <- function (
-    
+
   x,
   ...
-  
+
 ) {
-  
+
   n_digits = 1
-  
+
   dose_levels <- x[[1]]$dose_levels
   dose_names  <- names(attr(x, "posterior"))
-  
+
   predictions <- t(sapply(x, function (y) y$pred_values))
   colnames(predictions) <- dose_names
-  
+
   out_table <- data.frame(predictions,
                           mEff = sapply(x, function (y) y$max_effect),
                           gAIC = sapply(x, function (y) y$gAIC),
                           w    = sapply(x, function (y) y$model_weight))
   out_table <- apply(as.matrix(out_table), 2, round, digits = n_digits)
-  
+
   if (!is.null(x[[1]]$significant)) {
-    
+
     out_table <- as.data.frame(out_table)
     out_table$Sign <- sapply(x, function (y) y$significant)
-    
+
   }
-  
+
   model_names <- names(x) |>
     gsub("exponential", "exponential", x = _) |>
     gsub("quadratic",   "quadratic  ", x = _) |>
@@ -173,18 +173,18 @@ print.modelFits <- function (
     gsub("logistic",    "logistic   ", x = _) |>
     gsub("emax",        "emax       ", x = _) |>
     gsub("sigEmax",     "sigEmax    ", x = _)
-  
+
   # cat("Bayesian MCP Model Fits\n\n")
   cat("Model Coefficients\n")
   for (i in seq_along(model_names)) {
-    
+
     coeff_values <- x[[i]]$coeff
     coeff_names  <- names(coeff_values)
-    
+
     cat(model_names[i],
         paste(coeff_names, round(coeff_values, n_digits), sep = " = "), "\n",
         sep = " ")
-    
+
   }
   cat("\n")
   cat("Dose Levels\n",
@@ -192,7 +192,7 @@ print.modelFits <- function (
   cat("\n")
   cat("Predictions, Maximum Effect, gAIC, Model Weights & Significance\n")
   print(out_table, ...)
-  
+
 }
 
 ## plot.ModelFits()
@@ -202,57 +202,57 @@ print.modelFits <- function (
 
 #' @export
 summary.postList <- function (
-    
+
   object,
   ...
-  
+
 ) {
-  
+
   summary_list        <- lapply(object, summary, ...)
   names(summary_list) <- names(object)
   summary_tab         <- do.call(rbind, summary_list)
-  
+
   return (summary_tab)
-  
+
 }
 
 #' @export
 print.postList <- function (
-    
+
   x,
   ...
-  
+
 ) {
-  
+
   getMaxDiff <- function (
-    
+
     medians
-    
+
   ) {
-    
+
     diffs <- medians - medians[1]
-    
+
     max_diff       <- max(diffs)
     max_diff_level <- which.max(diffs) - 1
-    
+
     out <- c(max_diff, max_diff_level)
     names(out) <- c("max_diff", "DG")
-    
+
     return (out)
-    
+
   }
-  
+
   summary_tab <- summary.postList(x)
-  
+
   names(x) <- rownames(summary_tab)
   class(x) <- NULL
-  
+
   list_out <- list(summary_tab, getMaxDiff(summary_tab[, 4]), x)
   names(list_out) <- c("Summary of Posterior Distributions",
                        "Maximum Difference to Control and Dose Group",
                        "Posterior Distributions")
-  
+
   print(list_out, ...)
-  
+
 }
 
