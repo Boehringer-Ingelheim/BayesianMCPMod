0e9b4c43382f67b588324424f02b2c541909b5b9
Andersen <andersla@inter-standard-dy-cpr-interactive-st-15.workbenchhpc5795f00e20240704114050.pcluster>
2024-07-23 15:57:02 +0200

    removed library call from tests and included testdata as RDS

--- tests/testthat/test-s3methods.R
+++ tests/testthat/test-s3methods.R
@@ -1,31 +1,27 @@
 test_that("print.BayesianMCPMod works as intented", {
   expect_error(print.BayesianMCPMod())
-  
+
 })
 
 
 test_that("print.BayesianMCP works as intented", {
   expect_error(print.BayesianMCP())
-  
-  
-  
+
+
+
 })
 
 test_that("predict.ModelFits works as intented", {
   expect_error(predict.ModelFits())
-  
+
 })
 
 test_that("s3 postList functions work as intented", {
-  # setup
-  library(clinDR)
   library(dplyr)
   set.seed(8080)
-  data("metaData")
-  testdata    <- as.data.frame(metaData)
   dataset     <- filter(testdata, bname == "BRINTELLIX")
   histcontrol <- filter(dataset, dose == 0, primtime == 8, indication == "MAJOR DEPRESSIVE DISORDER",protid!=6)
-  
+
   ##Create MAP Prior
   hist_data <- data.frame(
     trial = histcontrol$nctno,
@@ -33,7 +29,7 @@ test_that("s3 postList functions work as intented", {
     se    = histcontrol$se,
     sd    = histcontrol$sd,
     n     = histcontrol$sampsize)
-  
+
   dose_levels <- c(0, 2.5, 5, 10)
   post_test_list <- getPriorList(
     hist_data = hist_data,
@@ -50,19 +46,19 @@ test_that("s3 postList functions work as intented", {
 })
 
 test_that("test modelFits s3 methods", {
-  
+
   model_shapes <- colnames(contr_mat$contMat)
   dose_levels  <- as.numeric(rownames(contr_mat$contMat))
-  
+
   model_fits  <- getModelFits(
     models      = model_shapes,
     dose_levels = dose_levels,
     posterior   = posterior_list,
     simple      = simple)
-  
+
   pred <- predict(model_fits)
   pred_dosage <- predict(model_fits, doses = dose_levels)
-  
+
   expect_type(pred, "list")
   expect_true(is.null(attr(pred, "doses")))
   expect_identical(attr(pred_dosage, "doses"), dose_levels)
